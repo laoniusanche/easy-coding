3032c66e77477132826020b825626ea0306794cc
luhanlin <47901673@qq.com>
2018-11-07 10:06:52 +0800

    Mr_lu 提交工程 初始化工程

---
+++ src/main/java/com/luhanlin/easycoding/EasyCodingApplication.java
@@ -0,0 +1,12 @@
+package com.luhanlin.easycoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCodingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/luhanlin/easycoding/EasyCodingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luhanlin.easycoding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyCodingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
